4060191cc350f9b9295204dab8bac9e2b9157edd
Foisal Ahamed Anik <foisalahamedanik@gmail.com>
2025-09-17 17:44:44 +0600

    add comments and changes

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 from fastapi import FastAPI
 app = FastAPI()
-
+#Request Get Method Url
+@app.get("/posts")
+async def root():
+    return {"message": "post"}
 @app.get("/")
 async def root():
     return {"message": "Welcome to my api"}
-@app.get("/post/")
-async def root():
-    return {"message": "post"}
+
