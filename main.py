0a143cde6eadd0c314c4beaa414da563e7c5ba34
Foisal Ahamed Anik <foisalahamedanik@gmail.com>
2025-09-17 19:12:47 +0600

    Adding a post function name Create_posts with url /createposts

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 from fastapi import FastAPI
+from fastapi.params import Body
 app = FastAPI()
 #Request Get Method Url
 @app.get("/posts")
@@ -8,3 +9,7 @@ async def root():
 async def root():
     return {"message": "Welcome to my api"}
 
+@app.post("/createposts")
+def create_posts(payLoad: dict = Body(...)):
+    print(payLoad)
+    return {"new_post": f"title {payLoad['title']} content: {payLoad['content']}"}
